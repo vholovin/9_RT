7e4caf15a88c11a15a68c01448fbde293b24e99f
bruteflow <bruteflow@iMac-bruteflow.local>
2017-10-23 18:33:25 +0300

    camera.0.0.1

--- src/events/keyboard_core.c
+++ src/events/keyboard_core.c
@@ -18,36 +18,54 @@ void	keyboard_core(t_rtv *rtv)
   		exit(0);
  	if (SDL_KEYDOWN && rtv->sdl->key_state[SDL_SCANCODE_UP])
  	{
-		t_cylinder *c;
-		c = rtv->scene->objects[0]->type;
-		c->a.x += 50;
-		c->b.x += 50;
+//		t_cylinder *c;
+//		c = rtv->scene->objects[0]->type;
+//		c->a.x += 50;
+//		c->b.x += 50;
+
+//		float t = vec3_dot(&rtv->scene->cam.eye, &rtv->scene->cam.d);
+
+		t_vec3d tmp = vec3_scale(0.06, &rtv->scene->cam.d);
+		rtv->scene->cam.eye = vec3_add(&rtv->scene->cam.eye, &tmp);
+		rtv->scene->cam.d = vec3_norm(rtv->scene->cam.eye);
+//		rtv->scene->cam.eye.z += 0.04;
+
+//		position += direction * speed
+//		rtv->scene->cam.d.z
+
 		clear_window(rtv);
 		raytrace(rtv);
 		render_present(rtv);
 	}
 	if (SDL_KEYDOWN && rtv->sdl->key_state[SDL_SCANCODE_DOWN])
 	{
-		t_cylinder *c;
-		c = rtv->scene->objects[0]->type;
-		c->a.x -= 50;
-		c->b.x -= 50;
+//		t_cylinder *c;
+//		c = rtv->scene->objects[0]->type;
+//		c->a.x -= 50;
+//		c->b.x -= 50;
+		rtv->scene->cam.eye.z -= 0.04;
+
 		clear_window(rtv);
 		raytrace(rtv);
 		render_present(rtv);
 	}
 	if (SDL_KEYDOWN && rtv->sdl->key_state[SDL_SCANCODE_RIGHT])
 	{
-		t_plane *p = rtv->scene->objects[0]->type;
-		p->normal.x += 50;
+//		t_plane *p = rtv->scene->objects[0]->type;
+//		p->normal.x += 50;
+
+		rtv->scene->cam.eye.x -= 0.04;
+
 		clear_window(rtv);
 		raytrace(rtv);
 		render_present(rtv);
 	}
 	if (SDL_KEYDOWN && rtv->sdl->key_state[SDL_SCANCODE_LEFT])
 	{
-		t_plane *p = rtv->scene->objects[0]->type;
-		p->normal.x -= 50;
+		rtv->scene->cam.eye.x += 0.04;
+//		t_plane *p = rtv->scene->objects[0]->type;
+//		p->normal.x -= 50;
+
 		clear_window(rtv);
 		raytrace(rtv);
 		render_present(rtv);
